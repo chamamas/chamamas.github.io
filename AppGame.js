14e20824b8465733d9d7a3cca0a82bb0af78d79b
chamamas <chamamas.k@ku.th>
2018-02-05 16:12:11 +0700

    make it middle

--- AppGame.js
+++ AppGame.js
@@ -1,25 +1,25 @@
-
-
-var i=0;
-var correctCount=0;
-function generate(index){
-	document.getElementById("question").innerHTML=jsonData[index].question;
-	document.getElementById("choice1").innerHTML=jsonData[index].choice1;
-	document.getElementById("choice2").innerHTML=jsonData[index].choice2;
-	document.getElementById("choice3").innerHTML=jsonData[index].choice3;
-	document.getElementById("choice4").innerHTML=jsonData[index].choice4;
-}
-function checkAnswer(){
-	if (document.getElementById()).checked && jsonData[i].choice1==jsonData[i].answer){
-		correctCount++;
-	}
-	if (document.getElementById()).checked && jsonData[i].choice2==jsonData[i].answer){
-		correctCount++;
-	}
-	if (document.getElementById()).checked && jsonData[i].choice3==jsonData[i].answer){
-		correctCount++;
-	}
-	if (document.getElementById()).checked && jsonData[i].choice4==jsonData[i].answer){
-		correctCount++;
-	}
-}
+//
+//
+// var i=0;
+// var correctCount=0;
+// function generate(index){
+// 	document.getElementById("question").innerHTML=jsonData[index].question;
+// 	document.getElementById("choice1").innerHTML=jsonData[index].choice1;
+// 	document.getElementById("choice2").innerHTML=jsonData[index].choice2;
+// 	document.getElementById("choice3").innerHTML=jsonData[index].choice3;
+// 	document.getElementById("choice4").innerHTML=jsonData[index].choice4;
+// }
+// function checkAnswer(){
+// 	if (document.getElementById()).checked && jsonData[i].choice1==jsonData[i].answer){
+// 		correctCount++;
+// 	}
+// 	if (document.getElementById()).checked && jsonData[i].choice2==jsonData[i].answer){
+// 		correctCount++;
+// 	}
+// 	if (document.getElementById()).checked && jsonData[i].choice3==jsonData[i].answer){
+// 		correctCount++;
+// 	}
+// 	if (document.getElementById()).checked && jsonData[i].choice4==jsonData[i].answer){
+// 		correctCount++;
+// 	}
+// }
